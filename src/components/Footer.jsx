f724c085da7f3bc2136f0305d26de9e9124ec6e9
Andres Garza <hanato21@icloud.com>
2023-12-08 12:08:56 -0600

    Almost finished, working on page links and mobile responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,115 @@
 import React from "react";
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>
-      <p></p>
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+      <div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+        <p className="py-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit
+          ullam iste repellat consequatur libero reiciendis, blanditiis
+          accusantium.
+        </p>
+        <div className="flex justify-between md:w-[75%] my-6">
+          <FaFacebookSquare
+            className="hover:cursor-pointer hover:text-[#00df9a]"
+            size={30}
+          />
+          <FaInstagram
+            className="hover:cursor-pointer hover:text-[#00df9a]"
+            size={30}
+          />
+          <FaTwitterSquare
+            className="hover:cursor-pointer hover:text-[#00df9a]"
+            size={30}
+          />
+          <FaGithubSquare
+            className="hover:cursor-pointer hover:text-[#00df9a]"
+            size={30}
+          />
+          <FaDribbbleSquare
+            className="hover:cursor-pointer hover:text-[#00df9a]"
+            size={30}
+          />
+        </div>
+      </div>
+      <div className="lg:col-span-2 flex justify-between mt-6">
+        <div>
+          <h6 className="font-medium text-gray-400">Solutions</h6>
+          <ul>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Analytics
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Marketing
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Commerce
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Insights
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Support</h6>
+          <ul>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Pricing
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Documentation
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Guides
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              API Status
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Company</h6>
+          <ul>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              About
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Blog
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Jobs
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Press
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Careers
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Legal</h6>
+          <ul>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Claim
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Policy
+            </li>
+            <li className="py-2 text-sm hover:cursor-pointer hover:text-gray-50">
+              Terms
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,33 +10,49 @@ const Navbar = () => {
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
+        <li className="p-4 hover:text-[#00df9a] hover:cursor-pointer">
+          <a>Home</a>
+        </li>
+        <li className="p-4 hover:text-[#00df9a] hover:cursor-pointer">
+          <a>Company</a>
+        </li>
+        <li className="p-4 hover:text-[#00df9a] hover:cursor-pointer">
+          <a>Resources</a>
+        </li>
+        <li className="p-4 hover:text-[#00df9a] hover:cursor-pointer">
+          <a>About</a>
+        </li>
+        <li className="p-4 hover:text-[#00df9a] hover:cursor-pointer">
+          <a>Contact</a>
+        </li>
       </ul>
       <div onClick={handleNav} className="block md:hidden hover:cursor-pointer">
-        {nav === false ? (
-          <AiOutlineClose size={20} />
-        ) : (
-          <AiOutlineMenu size={20} />
-        )}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
       <div
         className={
-          nav === false
+          nav
             ? "fixed left-0 top-0 h-full w-[60%] border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
             : "fixed left-[-100%]"
         }
       >
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
         <ul className="uppercase p-4">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resources</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4">Contact</li>
+          <li className="p-4 border-b border-gray-600 hover:text-[#00df9a] hover:cursor-pointer">
+            <a>Home</a>
+          </li>
+          <li className="p-4 border-b border-gray-600 hover:text-[#00df9a] hover:cursor-pointer">
+            <a>Company</a>
+          </li>
+          <li className="p-4 border-b border-gray-600 hover:text-[#00df9a] hover:cursor-pointer">
+            <a>Resources</a>
+          </li>
+          <li className="p-4 border-b border-gray-600 hover:text-[#00df9a] hover:cursor-pointer">
+            <a>About</a>
+          </li>
+          <li className="p-4 hover:text-[#00df9a] hover:cursor-pointer">
+            <a>Contact</a>
+          </li>
         </ul>
       </div>
     </div>
