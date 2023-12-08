68efffda5df23913ec50dc9a0c0840cdd1959fe3
Andres Garza <hanato21@icloud.com>
2023-12-08 13:10:16 -0600

    Final ver

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -17,7 +17,7 @@ const Analytics = () => {
             dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
             eveniet ex deserunt fuga?
           </p>
-          <button className="transition bg-black hover:bg-transparent hover:outline hover:outline-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 active:translate-y-[2px]">
+          <button className="transition bg-black hover:bg-transparent hover:outline hover:outline-1 hover:outline-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 active:translate-y-[2px]">
             Get Started
           </button>
         </div>
